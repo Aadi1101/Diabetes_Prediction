a4a1a666f8cedbcce6f39a756ad11f7ce6076963
Aadi1101 <aadityakomerwar@gmail.com>
2023-08-04 16:50:57 +0530

    Model training with Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import os,sys,time
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
 from src.logger import logging
 from src.exception import CustomException
 import dill,json
@@ -22,12 +22,18 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
             model = list(models.values())[i]
             logging.info(f"Evaluation initiated for {model}.")
             para = param[list(models.keys())[i]]
-            print(f"Para:{para}")
-            gs = GridSearchCV(model,para,cv=3,verbose=1)
+            # print(f"Para:{para}")
+            gs = GridSearchCV(model,para,cv=10,verbose=1,n_jobs=-1)
             logging.info(f"GridSearchCV initiated for {model}.")
             gs.fit(x_train,y_train)
             logging.info(f"GridSearchCV fit done and set_params initiated for {model}.")
             model.set_params(**gs.best_params_)
+
+            # rs = RandomizedSearchCV(estimator=model,param_distributions=para,cv=3,n_iter=100,random_state=100,n_jobs=-1)
+            # logging.info(f"RandomizedSearchCV initiated for {model}.")
+            # rs.fit(x_train,y_train)
+            # logging.info(f"RandomizedSearchCV fit done and set_params initiated for {model}.")
+            # model.set_params(**rs.best_params_)
             logging.info(f"setting parameters completed and fitting initiated for {model}.")
             model.fit(x_train,y_train)
             logging.info(f"prediction initiated for {model}.")
@@ -39,6 +45,7 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_accuracy
             end = time.time()
             logging.info(f"Obtained accuracy of {test_model_accuracy} and completed with {model}.")
+            # logging.info(f"Best params for the {model} are : {rs.best_params_} ")
         return report
     except Exception as e:
         raise CustomException(e,sys)
